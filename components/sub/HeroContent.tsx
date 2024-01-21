3a58e4ab2dd16afb78650563d17bda33b6410bc3
Franklin <71532004+fran1014@users.noreply.github.com>
2024-01-21 18:44:06 +0100

    adding motion effects to hero content

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -1,6 +1,9 @@
+'use client'
+
 import React from 'react'
 import {motion} from 'framer-motion'
-import { slideInFromTop } from '@/utils/motion'
+import { slideInFromLeft, slideInFromTop } from '@/utils/motion'
+import { SparklesIcon } from '@heroicons/react/16/solid'
 
 
 function HeroContent() {
@@ -12,7 +15,29 @@ function HeroContent() {
         <div className='h-full w-full flex flex-col gap-5 justify-center m-auto text-start'>
             <motion.div
             variants={slideInFromTop}
-            className='Welcome-box py-[15px] px-[4px] border border-[#7042f88b] opacity-[0.9] '></motion.div>
+            className='Welcome-box py-[8px] px-[6px] border border-[#7042f88b] opacity-[0.9] '>
+                <SparklesIcon className='text-[#b49bff] mr[10px] h-5 w-5 '/>
+                <h1 className='Welcome-text text-[13px]'>Blockchain, FullStack, Cloud Developer Portfolio</h1>
+            </motion.div>
+            <motion.div
+            variants={slideInFromLeft(0.5)}
+            className='flex flex-col gap-6 mt-6 text-6xl text-bold text-white max-w-[600px] w-auto h-auto'>
+              Providing
+              <span className='text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500'>the best
+              </span>
+              project experience
+            </motion.div>
+            <motion.p
+            variants={slideInFromLeft(0.8)}
+            className='text-lg text-gray-400 my-5 max-w-[600px]'>
+              I&apos;m a Blockchain, Full Stack & Cloud Software Engineer with experience in Web3, Tokens, Exchange, NFT, Automation Bots, Web2 and Cloud Software Development. Check out my projects and skills.
+            </motion.p>
+            <motion.a
+            variants={slideInFromLeft(1)}
+            className='py-2 button-primary text-center text-white cursor-pointer rounded-lg max-w-[200px]'>
+              Learn More!
+
+            </motion.a>
 
         </div>
       
