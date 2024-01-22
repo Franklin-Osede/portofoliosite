b1a638d6bbb43647b3568abcd53d61cca85aa1c0
Franklin <71532004+fran1014@users.noreply.github.com>
2024-01-22 23:34:00 +0100

    created navbar and style hero section motion effects

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -4,6 +4,8 @@ import React from 'react'
 import {motion} from 'framer-motion'
 import { slideInFromLeft, slideInFromTop } from '@/utils/motion'
 import { SparklesIcon } from '@heroicons/react/16/solid'
+import { slideInFromRight } from '../../utils/motion';
+import Image from 'next/image'
 
 
 function HeroContent() {
@@ -15,17 +17,18 @@ function HeroContent() {
         <div className='h-full w-full flex flex-col gap-5 justify-center m-auto text-start'>
             <motion.div
             variants={slideInFromTop}
-            className='Welcome-box py-[8px] px-[6px] border border-[#7042f88b] opacity-[0.9] '>
+            className='Welcome-box py-[8px] px-[7px] border border-[#7042f88b] opacity-[0.9] '>
                 <SparklesIcon className='text-[#b49bff] mr[10px] h-5 w-5 '/>
                 <h1 className='Welcome-text text-[13px]'>Blockchain, FullStack, Cloud Developer Portfolio</h1>
             </motion.div>
             <motion.div
             variants={slideInFromLeft(0.5)}
-            className='flex flex-col gap-6 mt-6 text-6xl text-bold text-white max-w-[600px] w-auto h-auto'>
+            className='flex flex-col gap-6 mt-6 text-6xl fort-bold text-white max-w-[600px] w-auto h-auto'>
+              <span>
               Providing
-              <span className='text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500'>the best
+              <span className='text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500'> the best  </span>
+               project experience
               </span>
-              project experience
             </motion.div>
             <motion.p
             variants={slideInFromLeft(0.8)}
@@ -40,6 +43,16 @@ function HeroContent() {
             </motion.a>
 
         </div>
+        <motion.div
+        variants={slideInFromRight(0.8)}
+        className='w-full h.full flex justify center items-center'>
+          <Image
+          src="/mainIconsdark.svg"
+          alt="work icons"
+          height={650}
+          width={650}/>
+
+        </motion.div>
       
     </motion.div>
   )
