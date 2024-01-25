00d069a25868c66394f12e03430f20537c0ed5b2
Franklin <franklin.op@hotmail.com>
2024-01-25 18:09:20 +0100

    adding links to sites

--- components/sub/ProjectCard.tsx
+++ components/sub/ProjectCard.tsx
@@ -1,28 +1,36 @@
-import Image from 'next/image'
-import React from 'react'
+import Image from 'next/image';
+import React from 'react';
 
-interface Props{
-    src:string;
-    title:string;
-    description:string
+interface Props {
+  src: string;
+  title: string;
+  description: string;
+  linkUrl: string;
 }
 
-function ProjectCard({src, title,description}:Props) {
+function ProjectCard({ src, title, description, linkUrl }: Props) {
   return (
-    <div className='relative overflow-hidden rounded-lg shadow-lg border border-[#2A0E61]'>
+    <a
+      href={linkUrl}
+      target="blank"
+      rel="noopener noreferrer"
+      className="max-w-[calc(33% -20px)]"
+    >
+      <div className="relative overflow-hidden rounded-lg shadow-lg border border-[#2A0E61]">
         <Image
-        src={src}
-        alt={title}
-        width={1000}
-        height={1000} 
-        className='w-full object-contain'/>
-        <div className='relative p-4'>
-            <h1 className='text-2xl font-semibold text-white'>{title} </h1>
-            <p className='mt-2 text-gray-300'> {description} </p>
+          src={src}
+          alt={title}
+          width={1000}
+          height={1000}
+          className="w-full object-contain"
+        />
+        <div className="relative p-4">
+          <h1 className="text-2xl font-semibold text-white">{title} </h1>
+          <p className="mt-2 text-gray-300"> {description} </p>
         </div>
-      
-    </div>
-  )
+      </div>
+    </a>
+  );
 }
 
-export default ProjectCard
+export default ProjectCard;
