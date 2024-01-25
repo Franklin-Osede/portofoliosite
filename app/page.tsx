38896768cef6a6d6b4658d9a60830b4d599c9fd1
Franklin <71532004+fran1014@users.noreply.github.com>
2024-01-25 12:35:20 +0100

    completed encryption part

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import Hero from "@/components/main/Hero"
 
 import Image from "next/image";
 import Skills from "@/components/main/Skills";
+import Encryption from "@/components/main/Encryption";
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
       <div className="flex flex-col gap-20">
         <Hero/>
       <Skills/>
+      <Encryption/>
         
       </div>
     </main>
