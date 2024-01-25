5d136c386f681669bd88b436c63026724ac3ff16
Franklin <71532004+fran1014@users.noreply.github.com>
2024-01-25 10:25:27 +0100

    added skils section and motion effect

--- components/main/Skills.tsx
+++ components/main/Skills.tsx
@@ -3,6 +3,7 @@
 import { Backend_skill, Frontend_skill, Full_stack,Other_skill } from '@/constants'
 import React from 'react'
 import SkillDataProvider from '../sub/SkillDataProvider'
+import SkillText from '../sub/SkillText'
 
 const Skills = () => {
   return (
@@ -10,6 +11,7 @@ const Skills = () => {
     id='skills'
     className='flex flex-col items-center justify-center gap-3 h-full relative overflow-hidden py-20'
     style={{transform:"scale(0.9)"}}>
+      <SkillText/>
         <div className='flex flex-row justify-around flex-wrap mt-4 gap-5 items-center'>
             {Frontend_skill.map((image,index)=>(
                 <SkillDataProvider
@@ -50,7 +52,18 @@ const Skills = () => {
                 index = {index} />
             ))}
         </div>
-        <div></div>
+        <div className='w-full h-full absolute'>
+          <div className='w-full h-full z-[-10] opacity-30 absolute flex items-center justify-center bg-cover'>
+            <video
+            className='w-full h-auto'
+            preload = 'false'
+            playsInline
+            loop
+            muted
+            autoPlay
+            src='/cards-video.webm'/>
+          </div>
+        </div>
 
     </section>
   )
