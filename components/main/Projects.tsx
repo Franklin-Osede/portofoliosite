343d8707cf834ec51a945b14bdb6a9072eb75268
Franklin <franklin.op@hotmail.com>
2024-02-15 17:20:32 +0100

    updating project cards

--- components/main/Projects.tsx
+++ components/main/Projects.tsx
@@ -9,10 +9,10 @@ function Projects() {
       </h1>
       <div className="flex flex-wrap justify-center gap-10 px-10">
         <ProjectCard
-          src="/NextWebsite.png"
-          title="Modern Next js Portfolio"
-          description="Lorem ipsum dolor sir amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ur labore et dolore magna alique"
-          linkUrl=""
+          src="/web3.png"
+          title="Web3 Decentralized Crypto Exchange-(Solidity,React, JavaScript, Hardhat, Ether js, Node js)"
+          description="DAap crypto exchange to trade tokens and interact with the Blockchain. Coded from scratch using different technologies."
+          linkUrl="https://cryptoexchange.on.fleek.co/"
         />
         <ProjectCard
           src="/CardImage.png"
@@ -21,23 +21,32 @@ function Projects() {
           linkUrl=""
         />
         <ProjectCard
+          src="/chia.png"
+          title="Decentralized App for blockchain transactions- (Mongo DB, Express, Angular, Node)"
+          description="Chia Network operates as a company focused on blockchain technology and smart transaction platforms. It includes a decentralized and secure blockchain and transaction platform."
+          linkUrl="https://www.chia.net/"
+        />
+        <ProjectCard
           src="/booking.png"
           title="Booking Clone Web Scraper- (React,Next.js, TypeScript, Tailwind, Shadcn)"
-          description="Web scraping app, use in the ethical way to extract information from any site, using Oxylabs. The front of the app displays a clone of the booking.com site"
+          description="Web scraping app, use in the ethical way to extract information from any site, using Oxylabs. The front of the app displays a clone of the booking.com site."
           linkUrl="https://booking-clone-nextjs.netlify.app/"
         />
+
         <ProjectCard
           src="/SpaceWebsite.png"
           title="Phyton"
           description="Lorem ipsum dolor sir amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ur labore et dolore magna alique"
           linkUrl=""
         />
+
         <ProjectCard
-          src="/SpaceWebsite.png"
-          title="Phyton"
-          description="Lorem ipsum dolor sir amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ur labore et dolore magna alique"
-          linkUrl=""
+          src="/supportdesk.png"
+          title="Support Tikect App MERN Stack - (React- Node Js- Mongo DB)"
+          description="Support Ticket App with MERN technology with full authentication and CRUD features using Redux to manage the state. "
+          linkUrl="https://ticket-app-front.onrender.com"
         />
+
         <ProjectCard
           src="/SpaceWebsite.png"
           title="Phyton"
@@ -50,12 +59,7 @@ function Projects() {
           description="Paragraph is a SaaS App which is  platform that allows artist, writers, podcasters and beyond to build a sustain building and connecting with their community,"
           linkUrl="https://paragraph.xyz/"
         />
-        <ProjectCard
-          src="/chia.png"
-          title="Decentralized App for blockchain transactions- (Mongo DB, Express, Angular, Node)"
-          description="Chia Network operates as a company focused on blockchain technology and smart transaction platforms. It includes a decentralized and secure blockchain and transaction platform."
-          linkUrl="https://www.chia.net/"
-        />
+
         <ProjectCard
           src="/hotelapp.png"
           title="Hotel booking portal application- (Angular)"
