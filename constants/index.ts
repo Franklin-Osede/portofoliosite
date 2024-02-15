b29302322dc06f73b48612935ce173d961556769
Franklin <franklin.op@hotmail.com>
2024-02-15 19:57:00 +0100

    updated all files and completed UI

---
+++ constants/index.ts
@@ -0,0 +1,246 @@
+export const Skill_data = [
+  {
+    skill_name: 'Html 5',
+    Image: '/html.png',
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: 'Css',
+    Image: '/css.png',
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: 'JavaScript',
+    Image: '/js.png',
+    width: 65,
+    height: 65,
+  },
+  {
+    skill_name: 'Tailwind Css',
+    Image: '/tailwind.png',
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: 'React',
+    Image: '/react.png',
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: 'Redux',
+    Image: '/redux.png',
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: 'Solidity',
+    Image: '/solidity.svg',
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: 'Rust',
+    Image: '/rust.png',
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: 'Ethereum',
+    Image: '/ethereum.png',
+    width: 60,
+    height: 60,
+  },
+  {
+    skill_name: 'Type Script',
+    Image: '/ts.png',
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: 'Next js',
+    Image: '/next.png',
+    width: 80,
+    height: 80,
+  },
+
+  {
+    skill_name: 'Mongo db',
+    Image: '/mongodb.png',
+    width: 40,
+    height: 40,
+  },
+  {
+    skill_name: 'SQL',
+    Image: '/sql.png',
+    width: 55,
+    height: 90,
+  },
+];
+
+export const Socials = [
+  {
+    name: 'Linkedin',
+    src: '/linkedin.png',
+    url: 'https://www.linkedin.com/in/franklinosede/',
+  },
+  {
+    name: 'GitHub',
+    src: '/github.jpeg',
+    url: 'https://github.com/fran1014',
+  },
+  {
+    name: 'Discord',
+    src: '/discord.svg',
+    url: 'https://discord.com/channels/@me',
+  },
+];
+
+export const Frontend_skill = [
+  {
+    skill_name: 'Html 5',
+    Image: '/html.png',
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: 'Css',
+    Image: '/css.png',
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: 'Java Script',
+    Image: '/js.png',
+    width: 65,
+    height: 65,
+  },
+  {
+    skill_name: 'Tailwind Css',
+    Image: '/tailwind.png',
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: 'Material UI',
+    Image: '/mui.png',
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: 'React',
+    Image: '/react.png',
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: 'Redux',
+    Image: '/redux.png',
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: 'Vue',
+    Image: '/vue.png',
+    width: 80,
+    height: 80,
+  },
+];
+
+export const Backend_skill = [
+  {
+    skill_name: 'Node js',
+    Image: '/node-js.png',
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: 'Express js',
+    Image: '/express.png',
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: 'Angular',
+    Image: '/angular.png',
+    width: 70,
+    height: 70,
+  },
+  {
+    skill_name: 'Google Cloud',
+    Image: '/googlecloud.png',
+    width: 70,
+    height: 70,
+  },
+  {
+    skill_name: 'AWS',
+    Image: '/aws.png',
+    width: 120,
+    height: 100,
+  },
+  {
+    skill_name: 'Postger SQL',
+    Image: '/postger.png',
+    width: 70,
+    height: 70,
+  },
+  {
+    skill_name: 'My SQL',
+    Image: '/mysql.png',
+    width: 70,
+    height: 70,
+  },
+
+  {
+    skill_name: 'SQL',
+    Image: '/graphql.png',
+    width: 80,
+    height: 80,
+  },
+];
+
+export const Full_stack = [
+  {
+    skill_name: 'React Native',
+    Image: '/ReactNative .png',
+    width: 70,
+    height: 70,
+  },
+
+  {
+    skill_name: 'Docker',
+    Image: '/docker.webp',
+    width: 70,
+    height: 70,
+  },
+
+  {
+    skill_name: 'PHP',
+    Image: '/php.png',
+    width: 90,
+    height: 90,
+  },
+  {
+    skill_name: 'Lavarel',
+    Image: '/lavarel.png',
+    width: 60,
+    height: 60,
+  },
+  {
+    skill_name: 'Go',
+    Image: '/go.png',
+    width: 60,
+    height: 60,
+  },
+];
+
+export const Other_skill = [
+  {
+    skill_name: 'Blockchain',
+    Image: '/blockchain.png',
+    width: 170,
+    height: 90,
+  },
+];
