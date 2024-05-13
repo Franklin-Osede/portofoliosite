21c008f405cbdee79af5234ebea8caf53cc3ac6c
Franklin <franklin.op@hotmail.com>
2024-05-13 17:43:30 +0200

    updated project cards

--- components/main/Projects.tsx
+++ components/main/Projects.tsx
@@ -10,31 +10,31 @@ function Projects() {
       <div className="flex flex-wrap justify-center gap-10 px-10">
         <ProjectCard
           src="/web3.png"
-          title="Web3 Decentralized Crypto Exchange-(Solidity , React js, JavaScript, Hardhat, Ether js, Node js)"
+          title="Web3 Decentralized Crypto Exchange-(React js, JavaScript, Node js, Solidity , Hardhat, Ether js)"
           description="DAap crypto exchange to trade tokens and interact with the Blockchain. Coded from scratch using different technologies allowing users mutilple trading interactions and functionalities."
           linkUrl="https://cryptoexchange.on.fleek.co/"
         />
         <ProjectCard
           src="/ticket.png"
-          title="Ticketmaster Events DAap - (Solidity, JavaScript, Node js, Ether js, Hardhat, React js)"
+          title="Ticketmaster Events DAap - (React js, JavaScript, Node js, Solidity, Ether js, Hardhat)"
           description="This application allows users to explore upcoming events, view available dates, and select and do transactions to receive a digital ticket in the form of a unique NFT directly into their wallet."
           linkUrl="https://tikeckmasterweb3.on.fleek.co/"
         />
         <ProjectCard
           src="/ecommerce.png"
-          title="Ecommerce DAap - (Solidity, JavaScript, React js, HardHat, Ether js) "
+          title="Ecommerce DAap - (React js, JavaScript, Solidity, HardHat, Ether js) "
           description="Ecommerce DAap to purchase through Blockchain technology. Each product purchase triggers a smart contract that manages the order, ensuring the merchant fulfills and delivers it on-chain."
           linkUrl="https://ecommercedaap.on.fleek.co/"
         />
         <ProjectCard
           src="/chia.png"
-          title="Decentralized App for blockchain transactions- (Mongo DB, Express, Angular, Node js)"
+          title="Decentralized App for blockchain transactions- (Angular, Node js, Express, Mongo DB)"
           description="Chia Network operates as a company focused on blockchain technology and smart transaction platforms. It includes a decentralized and secure blockchain and transaction platform."
           linkUrl="https://www.chia.net/"
         />
         <ProjectCard
           src="/booking.png"
-          title="Booking Clone Web Scraper- (React,Next.js, TypeScript, Tailwind, Shadcn)"
+          title="Booking Clone Web Scraper- (Next js, React js, TypeScript, Tailwind, Shadcn)"
           description="Web scraping app, use in the ethical way to extract information from any site, using Oxylabs. The front of the app displays a clone of the booking.com site."
           linkUrl="https://booking-clone-nextjs.netlify.app/"
         />
@@ -48,7 +48,7 @@ function Projects() {
 
         <ProjectCard
           src="/supportdesk.png"
-          title="Support Tikect App MERN Stack - (React- Node Js- Mongo DB)"
+          title="Support Tikect App MERN Stack - (React js, Node js, Mongo DB)"
           description="Support Ticket App with MERN technology with full authentication and CRUD features using Redux to manage the state. "
           linkUrl="https://ticket-app-front.onrender.com"
         />
@@ -61,7 +61,7 @@ function Projects() {
         />
         <ProjectCard
           src="/contentcreation.png"
-          title="Full-Stack Content AI -(Mongo DB, Express js, React, Node)"
+          title="Full-Stack Content AI -(React js, Node js, Express js, Mongo DB)"
           description="Paragraph is a SaaS App which is  platform that allows artist, writers, podcasters and beyond to build a sustain building and connecting with their community."
           linkUrl="https://paragraph.xyz/"
         />
@@ -74,7 +74,7 @@ function Projects() {
         />
         <ProjectCard
           src="/insurance.png"
-          title="Insurance Comparision SaaS - (Vue js, Next js, SQL)"
+          title="Insurance Comparision SaaS - (Vue js, SQL)"
           description="Beshak is a mission to bring resources, tools and experts that ensures you are never puzzled, and buy insurance with confidence."
           linkUrl="https://www.beshak.org/"
         />
