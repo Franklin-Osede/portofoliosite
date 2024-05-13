dd72c72974139fe06e2bac2ddad8997d29456040
Franklin <franklin.op@hotmail.com>
2024-05-13 17:50:15 +0200

    added playInline functionality

--- components/main/Hero.tsx
+++ components/main/Hero.tsx
@@ -8,6 +8,7 @@ const Hero = () => {
         autoPlay
         muted
         loop
+        playsInline
         className='rotate-180 absolute top-[-340px] left-0 z-[1] w-full h-full object-cover'>
             <source src='/blackhole.webm' type='video/webm'/>
         </video>
